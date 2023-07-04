327bb1b3bed8b3f961f9cfdbf066292ad662a348
Jérémy chassin <jerem.chassin@gmail.com>
2023-07-05 01:42:35 +0200

    Mise en page et premier projet

--- src/app/project-card/project-card.component.ts
+++ src/app/project-card/project-card.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, AfterViewInit, ViewChild, ElementRef  } from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.css']
 })
-export class ProjectCardComponent implements OnInit {
+export class ProjectCardComponent implements OnInit, AfterViewInit {
   @Input() project: any;
-  @Output() openNotebook = new EventEmitter<string>();
+  @Output() openUrl = new EventEmitter<string>();
+  @ViewChild('projectImage', { static: false })
+  projectImage!: ElementRef;
 
   constructor() { }
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.adjustImageHeight();
+  }
+  
+  adjustImageHeight() {
+    const element = this.projectImage.nativeElement;
+    const image = new Image();
+    image.src = element.style.backgroundImage.replace(/url\(['"]?(.*?)['"]?\)/i, '$1');
+
+    image.onload = () => {
+      const aspectRatio = image.width / image.height;
+      const containerWidth = element.offsetWidth;
+      const calculatedHeight = containerWidth / aspectRatio;
+      element.style.height = `${calculatedHeight}px`;
+    };
   }
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -11,64 +11,22 @@ export class ProjectsComponent implements OnInit {
 
   projects = [
     {
-      name: 'Project 1',
-      thumbnail: 'path-to-thumbnail-image-1',
-      description: 'Brief description of Project 1 Brief description of Project 1 Brief description of Project 1 Brief description of Project 1 Brief description of Project 1 Brief description of Project 1 Brief description of Project 1 Brief description of Project 1 Brief description of Project 1 Brief description of Project 1 Brief description of Project 1 Brief description of Project 1 Brief description of Project 1 ',
-      notebookUrl: 'link-to-project-1-notebook'
-    },
-    {
-      name: 'Project 2',
-      thumbnail: 'path-to-thumbnail-image-2',
-      description: 'Brief description of Project 2',
-      notebookUrl: 'link-to-project-2-notebook'
-    },
-    {
-      name: 'Project 3',
-      thumbnail: 'path-to-thumbnail-image-2',
-      description: 'Brief description of Project 2',
-      notebookUrl: 'link-to-project-2-notebook'
-    },
-    {
-      name: 'Project 4',
-      thumbnail: 'path-to-thumbnail-image-2',
-      description: 'Brief description of Project 2',
-      notebookUrl: 'link-to-project-2-notebook'
-    },
-    {
-      name: 'Project 5',
-      thumbnail: 'path-to-thumbnail-image-2',
-      description: 'Brief description of Project 2',
-      notebookUrl: 'link-to-project-2-notebook'
-    },
-    {
-      name: 'Project 6',
-      thumbnail: 'path-to-thumbnail-image-2',
-      description: 'Brief description of Project 2',
-      notebookUrl: 'link-to-project-2-notebook'
-    },
-    {
-      name: 'Project 7',
-      thumbnail: 'path-to-thumbnail-image-2',
-      description: 'Brief description of Project 2',
-      notebookUrl: 'link-to-project-2-notebook'
-    },
-    {
-      name: 'Project 8',
-      thumbnail: 'path-to-thumbnail-image-2',
-      description: 'Brief description of Project 2',
-      notebookUrl: 'link-to-project-2-notebook'
-    },
-    {
-      name: 'Project 9',
-      thumbnail: 'path-to-thumbnail-image-2',
-      description: 'Brief description of Project 2',
-      notebookUrl: 'link-to-project-2-notebook'
+      name: 'Image classifier performance improvement with data augmentation using Object detection',
+      thumbnail: '../../assets/ResNetYOLOClassifier.png',
+      technologies:'YOLOv8, CNN, ResNet 50, TensorFlow Keras, Tkinter',
+      description: `I used a pretrained Image Classifer to classify dogs breeds, added few layers and gave it an image generator for training.
+      Images were polluted by different elements like snow on Huskies pictures (another breed dog looking like a Husky in snow could be classifed as one) so I cropped the dogs for the model to focus on them.
+      Doing so allowed me to multiply the images when there was multiple dogs on them.
+      I served the model on a desktop app`,
+      dataset:'Stanford dogs',
+      datasetUrl:'http://vision.stanford.edu/aditya86/ImageNetDogs/',
+      notebookUrl: 'https://colab.research.google.com/drive/1_FUts9YnuVnUn_6uI3_-Z4Wgex5PZ7Tm?usp=sharing',
+      githubUrl: 'https://github.com/jerem64/InterfaceDogBreedClassifier'
     }
-    // Add more projects as needed
   ];
 
-  openNotebook(notebookUrl: string) {
+  openUrl(url: string) {
     // Open the Google Colaboratory notebook in a new tab
-    window.open(notebookUrl, '_blank');
+    window.open(url, '_blank');
   }
 }
