86147858e8a37aa78678353049ecabd1eacd7b21
Jérémy chassin <jerem.chassin@gmail.com>
2023-07-20 21:56:18 +0200

    lien bot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from './shared.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private sharedService: SharedService) {}
+
+  openChatbot(): void {
+    this.sharedService.openChatbot();
+  }
+
+  ngOnInit(): void {
+  }
   title = 'Jérémy Chassin';
 
   selectedItem: string = 'home';
